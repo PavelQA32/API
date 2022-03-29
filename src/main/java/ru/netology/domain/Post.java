43fd400cbe2c5e0504a81eb39b7d1dc7f72c9481
PS <ps-667@mail.ru>
2022-03-29 12:17:36 +0300

    API

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+public class Post {
+    String name;
+    String passport;
+    String patronymic;
+    String phone;
+    String surname;
+    Boolean subscription;
+
+    FormDate birthday = new FormDate();
+
+}
